562bfb8b3318743b4b5c4d69e50daca9d6c0854e
Yuri Ershov <shtorm0606@gmail.com>
2022-01-09 11:15:18 +0300

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Yuri Ershov on 9.01.22.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
